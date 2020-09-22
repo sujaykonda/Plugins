a47f753f87b743c383f09ea3e0e12263382afecc
sujaykonda <50930165+sujaykonda@users.noreply.github.com>
2020-09-22 12:20:04 -0400

    Few Changes

--- CapTheFlag/src/me/_xGQD/CapTheFlag/Commands/CTFCommand.java
+++ CapTheFlag/src/me/_xGQD/CapTheFlag/Commands/CTFCommand.java
@@ -15,7 +15,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import me._xGQD.CapTheFlag.Main;
-import me._xGQD.CapTheFlag.Functions;
+import me._xGQD.CapTheFlag.Classes.GameMap;
 
 public class CTFCommand implements CommandExecutor{
 	
@@ -25,84 +25,206 @@ public class CTFCommand implements CommandExecutor{
 		this.plugin = plugin;
 		
 		plugin.getCommand("ctf").setExecutor(this);
+		
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(args.length == 0) {
+			sender.sendMessage("CAPTURE THE FLAG");
+        	sender.sendMessage("---------------------");
+        	sender.sendMessage("/ctf maps - shows all maps");
+        	sender.sendMessage("/ctf join [map] - teleports you to that map");
+        	sender.sendMessage("/ctf help [map] - help message");
+        	if(sender.hasPermission("capturetheflag.*")) {
+	        	sender.sendMessage("/ctf start [map] - starts the game for that map");
+	        	sender.sendMessage("/ctf end [map] - ends the game for that map");
+	        	sender.sendMessage("/ctf open [map] - allows people to do /ctf join for that map");
+	        	sender.sendMessage("/ctf close [map] - stops people from joining the map");
+	        	sender.sendMessage("/ctf create [map] - creates a new map");
+	        	sender.sendMessage("/ctf delete [map] - deletes a new map");
+	        	sender.sendMessage("/ctf setspawn [map] [1 or 2](the side) - sets the spawn for the map");
+        	}
+		}
 		if(args.length >= 1){
 			String subcmd = args[0].toLowerCase();
 			switch (subcmd){
-		    	case "play":
-		    		if (!(sender instanceof Player)) {
-		    			sender.sendMessage("Only players can execute this command");
-						return true;
+				case "maps":
+					for(GameMap map : plugin.maps) {
+						sender.sendMessage(map.name);
 					}
-		    		Player player = (Player) sender;
-		    		if(player.hasPermission("capturetheflag.play")) {
-			    		if(plugin.open) {
-				    		if(plugin.team_2.size() >= plugin.team_1.size()) {
-				    			if(!plugin.team_2.contains(player.getUniqueId())) {
-				    				Functions.Box1(plugin, Material.BARRIER);
-				    				plugin.team_1.add(player.getUniqueId());
-				    				Functions.GivePlayerRed(player, plugin);
-				              
-				    			} else {
-				    				Functions.Box2(plugin, Material.BARRIER);
-				    				Functions.GivePlayerBlue(player, plugin);
-				    			}
-				    		}else{
-				    			if(!plugin.team_1.contains(player.getUniqueId())) {
-				    				Functions.Box2(plugin, Material.BARRIER);
-				    				plugin.team_2.add(player.getUniqueId());
-				    				Functions.GivePlayerBlue(player, plugin);
-				              
-				    			}else {
-				    				Functions.Box1(plugin, Material.BARRIER);
-				    				Functions.GivePlayerRed(player, plugin);
-				    			}
-				    		}
-				    		plugin.gold.put(player.getUniqueId(), 0);
-				    		plugin.buffs.put(player.getUniqueId(), new ArrayList<String>());
-				    		plugin.boards.get(player.getUniqueId()).updateLine(3, "Gold: " + 0);;
+					break;
+		    	case "join":
+		    		if(args.length == 2) {
+			    		if (!(sender instanceof Player)) {
+			    			sender.sendMessage("Only players can execute this command");
+							return true;
+						}
+			    		Player player = (Player) sender;
+			    		if(player.hasPermission("capturetheflag.play")) {
+			        		for(int i = 0; i < plugin.maps.size(); i++) {
+			        			GameMap map = plugin.maps.get(i);
+								if(map.name.equalsIgnoreCase(args[1])) {
+									if(map.open) {
+							    		plugin.buffs.put(player.getUniqueId(), new ArrayList<String>());
+							    		if(map.team_2.size() >= map.team_1.size()) {
+							    			if(!map.team_2.contains(player.getUniqueId())) {
+							    				map.Box1(Material.BARRIER);
+							    				map.team_1.add(player.getUniqueId());
+							    				map.GivePlayerRed(player, plugin);
+									    		plugin.boards.get(player.getUniqueId()).updateLine(3, "Gold: " + 0);
+							              
+							    			} else {
+							    				map.Box2(Material.BARRIER);
+							    				map.GivePlayerBlue(player, plugin);
+							    			}
+							    		}else{
+							    			if(!map.team_1.contains(player.getUniqueId())) {
+							    				map.Box2(Material.BARRIER);
+							    				map.team_2.add(player.getUniqueId());
+							    				map.GivePlayerBlue(player, plugin);
+									    		plugin.boards.get(player.getUniqueId()).updateLine(3, "Gold: " + 0);
+							              
+							    			}else {
+							    				map.Box1(Material.BARRIER);
+							    				map.GivePlayerRed(player, plugin);
+							    			}
+							    		}
+							    		plugin.gold.put(player.getUniqueId(), 0);
+									}
+						         	plugin.maps.set(i, map);
+								}
+							}
 			    		}else {
-			    			player.sendMessage("The game is not open");
+			    			player.sendMessage("You dont have permission to do that");
 			    		}
-		    		}else {
-		    			player.sendMessage("You dont have permission to do that");
 		    		}
 		    		break;
 		    	case "open":
-		    		if(sender.hasPermission("capturetheflag.*")) {
-		    			plugin.open = true;
-		    			sender.sendMessage("You opened the game");
-		    		}else {
-		    			sender.sendMessage("You dont have permission to do that");
+		    		if(args.length == 2) {
+			    		if(sender.hasPermission("capturetheflag.*")) {
+			        		for(int i = 0; i < plugin.maps.size(); i++) {
+			        			GameMap map = plugin.maps.get(i);
+								if(map.name.equalsIgnoreCase(args[1])) {
+									map.open = true;
+						         	plugin.maps.set(i, map);
+								}
+							}
+			    			
+			    			sender.sendMessage("You opened the game");
+			    		}else {
+			    			sender.sendMessage("You dont have permission to do that");
+			    		}
 		    		}
 		    		break;
 		    	case "close":
 		    		if(sender.hasPermission("capturetheflag.*")) {
-		    			plugin.open = false;
+		        		for(int i = 0; i < plugin.maps.size(); i++) {
+		        			GameMap map = plugin.maps.get(i);
+							if(map.name.equalsIgnoreCase(args[1])) {
+								map.open = false;
+					         	plugin.maps.set(i, map);
+							}
+						}
 		    			sender.sendMessage("You closed the game");
 		    		}else {
 		    			sender.sendMessage("You dont have permission to do that");
 		    		}
 		    		break;
 		        case "start":
-		        	plugin.open = false;
-		         	plugin.start = true;
-    				Functions.Box1(plugin, Material.AIR);
-    				Functions.Box2(plugin, Material.AIR);
-		         	
-		         	sender.sendMessage("You started the game");
+		        	if(sender.hasPermission("capturetheflag.*")) {
+		        		for(int i = 0; i < plugin.maps.size(); i++) {
+		        			GameMap map = plugin.maps.get(i);
+							if(map.name.equalsIgnoreCase(args[1])) {
+								map.open = false;
+					         	map.start = true;
+					         	map.lastHit.clear();
+					         	map.team_1_caps = 0;
+					         	map.team_2_caps = 0;
+					         	map.Box1(Material.AIR);
+					         	map.Box2(Material.AIR);
+					         	plugin.maps.set(i, map);
+					         	sender.sendMessage("You started the game");
+							}
+						}
+		        	}else {
+		        		sender.sendMessage("You do not have permission to run this command");
+		        	}
 		         	break;
 		        case "end":
 		        	if(sender.hasPermission("capturetheflag.*")) {
-		        		Functions.End(plugin);
+		        		for(int i = 0; i < plugin.maps.size(); i++) {
+		        			GameMap map = plugin.maps.get(i);
+							if(map.name.equalsIgnoreCase(args[1])) {
+								map.End(plugin);
+					         	plugin.maps.set(i, map);
+							}
+						}
 						sender.sendMessage("You ended the game");
 		        	}else {
 		        		sender.sendMessage("You dont have permission to do that");
 		        	}
 					break;
+		        case "help":
+		        	sender.sendMessage("CAPTURE THE FLAG");
+		        	sender.sendMessage("---------------------");
+		        	sender.sendMessage("/ctf maps - shows all maps");
+		        	sender.sendMessage("/ctf join [map] - teleports you to that map");
+		        	sender.sendMessage("/ctf help [map] - help message");
+		        	if(sender.hasPermission("capturetheflag.*")) {
+			        	sender.sendMessage("/ctf start [map] - starts the game for that map");
+			        	sender.sendMessage("/ctf end [map] - ends the game for that map");
+			        	sender.sendMessage("/ctf open [map] - allows people to do /ctf join for that map");
+			        	sender.sendMessage("/ctf close [map] - stops people from joining the map");
+			        	sender.sendMessage("/ctf create [map] - creates a new map");
+			        	sender.sendMessage("/ctf delete [map] - deletes a new map");
+			        	sender.sendMessage("/ctf setspawn [map] [1 or 2](the side) - sets the spawn for the map");
+		        	}
+		        	break;
+		        case "create":
+		        	if(sender.hasPermission("capturetheflag.*")) {
+			        	if(args.length == 2) {
+			        		if (plugin.config.contains(args[1])) {
+			        			sender.sendMessage("Already a map");
+			        		}else {
+			        			plugin.maps.add(new GameMap(args[1]));
+			        			plugin.config.createSection(args[1]);
+			        			plugin.config.options().copyDefaults(true);
+			        			plugin.saveConfig();
+			        			sender.sendMessage("Done");
+			        		}
+			        	}else {
+			        		sender.sendMessage("Not the correct amount of arguments");
+			        	}
+		        	}else {
+		        		sender.sendMessage("You do not haver permssion to do this command");
+		        	}
+		        	
+		        	break;
+		        case "delete":
+		        	if(sender.hasPermission("capturetheflag.*")) {
+			        	if(args.length == 2) {
+			        		if (plugin.config.contains(args[1])) {
+			        			for(int i = 0; i < plugin.maps.size(); i++) {
+			        				if(plugin.maps.get(i).name.equalsIgnoreCase(args[1])) {
+			        					plugin.maps.remove(i);
+			        					plugin.config.set(args[1], null);
+					        			plugin.config.options().copyDefaults(true);
+					        			plugin.saveConfig();
+					        			sender.sendMessage("Done");
+			        				}
+			        			}
+			        		}else {
+			        			sender.sendMessage("Not a map");
+			        		}
+			        	}else {
+			        		sender.sendMessage("Not the correct amount of arguments");
+			        	}
+		        	}else {
+		        		sender.sendMessage("You do not have permission to do this command");
+		        	}
+		        	
+		        	break;
 		        case "setspawn":
 		        	if (!(sender instanceof Player)) {
 		        		sender.sendMessage("Only players can execute this command");
@@ -110,18 +232,36 @@ public class CTFCommand implements CommandExecutor{
 		        	}
 		        	Player player1 = (Player) sender;
 		        	if(player1.hasPermission("capturetheflag.*")){
-		        		if(args.length == 2){
-		        			if(args[1].equalsIgnoreCase("1")) {
-		        				plugin.spawn_loc_1 = player1.getLocation();
-		        				player1.sendMessage("Done");
-		        			}else {
-		        				plugin.spawn_loc_2 = player1.getLocation();
-		        				player1.sendMessage("Done");
-		        			}
-		        		}else{
-		        			player1.sendMessage("Not the correct amount of arguments");
-		        			return true;
-		        		}  
+		        		if(args.length == 3){
+		        			player1.sendMessage("Correct Number of args");
+			        		for(int i = 0; i < plugin.maps.size(); i++) {
+			        			GameMap map = plugin.maps.get(i);
+			        			player1.sendMessage(map.name);
+								if(map.name.equalsIgnoreCase(args[1])) {
+				        			if(args[2].equalsIgnoreCase("1")) {
+				        				map.spawn_loc_1 = player1.getLocation();
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_1_w", player1.getLocation().getWorld().getName());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_1_x", player1.getLocation().getX());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_1_y", player1.getLocation().getY());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_1_z", player1.getLocation().getZ());
+				        				player1.sendMessage("Done");
+				        			}else {
+				        				map.spawn_loc_2 = player1.getLocation();
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_2_w", player1.getLocation().getWorld().getName());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_2_x", player1.getLocation().getX());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_2_y", player1.getLocation().getY());
+				        				plugin.config.getConfigurationSection(args[1]).set("spawn_loc_2_z", player1.getLocation().getZ());
+				        				player1.sendMessage("Done");
+				        			}
+				        			plugin.maps.set(i, map);
+				        			plugin.config.options().copyDefaults(true);
+				        			plugin.saveConfig();
+				        		}
+							}
+			        	}else{
+			        		player1.sendMessage("Not the correct amount of arguments");
+			       			return true;
+			       		}  
 		        	}else{
 		        		player1.sendMessage("You do not have permission to set the spawn");
 		        		return true;
--- CapTheFlag/src/me/_xGQD/CapTheFlag/Main.java
+++ CapTheFlag/src/me/_xGQD/CapTheFlag/Main.java
@@ -1,5 +1,6 @@
 package me._xGQD.CapTheFlag;
 
+import java.io.ObjectInputFilter.Config;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,34 +10,48 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me._xGQD.CapTheFlag.Classes.GameMap;
 import me._xGQD.CapTheFlag.Commands.CTFCommand;
 import me._xGQD.CapTheFlag.FastBoard.FastBoard;
+import me._xGQD.CapTheFlag.Listeners.PositionListener;
 
 public class Main extends JavaPlugin{
 	
-	public Location spawn_loc_1;
-	public Location spawn_loc_2;
-	public Set<UUID> team_1 = new HashSet<UUID>();
-	public Set<UUID> team_2 = new HashSet<UUID>();
-	public Set<UUID> spawn_prot = new HashSet<UUID>();
-	public boolean start = false;
-	public boolean open = false;
 	
+	public List<GameMap> maps = new ArrayList<GameMap>();
 	public final Map<UUID, FastBoard> boards = new HashMap<>();
 	public final Map<UUID, Integer> gold = new HashMap<>();
 	public final Map<UUID, List<String>> buffs = new HashMap<>();
 	public Inventory shop;
+	public FileConfiguration config;
 	// Fired when plugin is first enabled
     @Override
     public void onEnable() {
+    	
+    	config = this.getConfig();
+    	
+    	for(String key : config.getKeys(false)) {
+            ConfigurationSection map = config.getConfigurationSection(key);
+            GameMap loadedMap = new GameMap(key);
+            if(map.contains("spawn_loc_1_w") && map.contains("spawn_loc_2_w")) {
+            	loadedMap.spawn_loc_1 = new Location(getServer().getWorld(map.getString("spawn_loc_1_w")), map.getDouble("spawn_loc_1_x"), map.getDouble("spawn_loc_1_y"), map.getDouble("spawn_loc_1_z"));
+            	loadedMap.spawn_loc_2 = new Location(getServer().getWorld(map.getString("spawn_loc_2_w")), map.getDouble("spawn_loc_2_x"), map.getDouble("spawn_loc_2_y"), map.getDouble("spawn_loc_2_z"));
+            }
+            maps.add(loadedMap);
+        }
     	
     	new CTFCommand(this);
     	new PositionListener(this);
@@ -47,12 +62,77 @@ public class Main extends JavaPlugin{
     	ItemStack permsword = new ItemStack(Material.GOLD_SWORD);
     	ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
     	ItemStack iron = new ItemStack(Material.IRON_CHESTPLATE);
-    	permsword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+    	ItemStack haste = new ItemStack(Material.GOLD_PICKAXE);
+    	ItemStack rod = new ItemStack(Material.FISHING_ROD);
+    	ItemStack snow = new ItemStack(Material.SNOW_BLOCK);
+    	
+    	permsword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+    	
+    	ItemMeta permchainmeta = permchain.getItemMeta();
+    	permchainmeta.setDisplayName("Chain Armor");
+    	List<String> permchainlore = new ArrayList<String>();
+    	permchainlore.add("Permanent Chain Mail Chestplate, Leggings, and Boots");
+    	permchainlore.add("Cost 100");
+    	permchainmeta.setLore(permchainlore);
+    	permchain.setItemMeta(permchainmeta);
+    	
+    	ItemMeta permswordmeta = permsword.getItemMeta();
+    	permswordmeta.setDisplayName("Sharp 2 Gold Sword");
+    	List<String> permswordlore = new ArrayList<String>();
+    	permswordlore.add("Permanent Sharpness 2 Gold Sword");
+    	permswordlore.add("Cost 50 Gold");
+    	permswordmeta.setLore(permswordlore);
+    	permsword.setItemMeta(permswordmeta);
+    	
+    	ItemMeta swordmeta = sword.getItemMeta();
+    	swordmeta.setDisplayName("Diamond Sword");
+    	List<String> swordlore = new ArrayList<String>();
+    	swordlore.add("Temporary Diamond Sword for until you die");
+    	swordlore.add("Cost 50 Gold");
+    	swordmeta.setLore(swordlore);
+    	sword.setItemMeta(swordmeta);
+    	
+    	ItemMeta ironmeta = iron.getItemMeta();
+    	ironmeta.setDisplayName("Iron Armor");
+    	List<String> ironlore = new ArrayList<String>();
+    	ironlore.add("Temporary Iron Chestplate, Leggings, and Boots");
+    	ironlore.add("Cost 100 Gold");
+    	ironmeta.setLore(ironlore);
+    	iron.setItemMeta(ironmeta);
+    	
+    	ItemMeta hastemeta = haste.getItemMeta();
+    	hastemeta.setDisplayName("Haste 3");
+    	List<String> hastelore = new ArrayList<String>();
+    	hastelore.add("Permanent Haste 3");
+    	hastelore.add("Cost 50 Gold");
+    	hastemeta.setLore(hastelore);
+    	haste.setItemMeta(hastemeta);
+    	
+    	ItemMeta rodmeta = rod.getItemMeta();
+    	rodmeta.setDisplayName("Fishing Rod");
+    	List<String> rodlore = new ArrayList<String>();
+    	rodlore.add("Temporary Fishing Rod");
+    	rodlore.add("Cost 30 Gold");
+    	rodmeta.setLore(rodlore);
+    	rod.setItemMeta(rodmeta);
+    	
+    	rod.setDurability((short) 10);
+    	
+    	ItemMeta snowmeta = snow.getItemMeta();
+    	snowmeta.setDisplayName("Snow Blocks");
+    	List<String> snowlore = new ArrayList<String>();
+    	snowlore.add("Temporary Snow Blocks Longer Break Time");
+    	snowlore.add("Cost 5 Gold (each)");
+    	snowmeta.setLore(snowlore);
+    	snow.setItemMeta(snowmeta);
     	
     	shop.setItem(0, permchain);
-    	shop.setItem(2, permsword);
-    	shop.setItem(4, iron);
-    	shop.setItem(6, sword);
+    	shop.setItem(1, permsword);
+    	shop.setItem(2, iron);
+    	shop.setItem(3, sword);
+    	shop.setItem(4, haste);
+    	shop.setItem(5, rod);
+    	shop.setItem(6, snow);
     	
     	getServer().getScheduler().runTaskTimer(this, () -> {
             for (FastBoard board : boards.values()) {
@@ -62,12 +142,21 @@ public class Main extends JavaPlugin{
     }
     
     private void updateBoard(FastBoard board) {
-    	board.updateLine(1, "Online: " + getServer().getOnlinePlayers().size());
+    	board.updateLine(1, ChatColor.RED + "Online: " + ChatColor.GRAY + getServer().getOnlinePlayers().size());
 	}
 
 	// Fired when plugin is disabled
     @Override
     public void onDisable() {
     	
+    }
+    
+    public GameMap getMap(Player player) {
+    	for(GameMap map: maps) {
+    		if(map.PlayerIn(player)) {
+    			return map;
+    		}
+    	}
+    	return null;
     }
 }
